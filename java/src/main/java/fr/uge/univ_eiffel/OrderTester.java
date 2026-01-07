78bd147a12d737662b72006f2b1322315a2bbd0b
redlinka <nachnouchiadam@gmail.com>
2026-01-07 19:30:59 +0100

    intermediaire

--- java/src/main/java/fr/uge/univ_eiffel/OrderTester.java
+++ java/src/main/java/fr/uge/univ_eiffel/OrderTester.java
@@ -9,6 +9,8 @@ import fr.uge.univ_eiffel.payment_methods.pow.PoWMethod;
 import fr.uge.univ_eiffel.security.BrickVerifier;
 import fr.uge.univ_eiffel.security.OfflineVerifier;
 
+import static java.sql.Types.NULL;
+
 public class OrderTester {
     public static void main(String[] args) {
 
@@ -17,19 +19,16 @@ public class OrderTester {
             final FactoryClient client = FactoryClient.makeFromProps("config.properties");
             final OrderManager orderer = new OrderManager(client, inventory);
             final PaymentMethod payer = new PoWMethod(client);
-
             String publicKey = client.signaturePublicKey();
             final BrickVerifier verifier = new OfflineVerifier(publicKey);
-
-            /*final RestockManager restorer = new RestockManager(inventory, client, orderer, payer, verifier);
+            final RestockManager restorer = new RestockManager(inventory, client, orderer, payer, verifier);
 
             String solutionPath = "output.txt";
+            int imageId = 1;
+            int lastId = inventory.newConfirmedTiling(solutionPath, imageId);
 
-            restorer.reactiveRestockage(solutionPath);*/
-            inventory.exportCatalog("catalog.txt");
+            restorer.reactiveRestockage(solutionPath, lastId);
 
         } catch (Exception e) {e.printStackTrace();}
-
-
     }
 }
