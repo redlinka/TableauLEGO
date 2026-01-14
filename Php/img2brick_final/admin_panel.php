2f2c6d1311eb7c5073fbe8b2d0c89bcd13c8f82e
redlinka <nachnouchiadam@gmail.com>
2026-01-14 20:38:40 +0100

    Update admin_panel.php

--- Php/img2brick_final/admin_panel.php
+++ Php/img2brick_final/admin_panel.php
@@ -256,7 +256,20 @@ foreach ($catalog as $item) {
                 <?php else: ?>
                     <?php foreach ($orders as $order): ?>
                         <div class="order-card">
-                            
+                            <div class="order-header">
+                                <div>
+                                    <strong>Order #<?= $order['order_id'] ?></strong><br>
+                                    <small>Placed on : <?= date('d/m/Y H:i', strtotime($order['created_at'])) ?></small><br>
+                                    <small>User: <?= htmlspecialchars($order['first_name'] . ' ' . $order['last_name']) ?> (<?= htmlspecialchars($order['email']) ?>)</small>
+                                </div>
+                                <div>
+                                    <span class="status-badge">Paid</span>
+                                </div>
+                            </div>
+
+                            <div class="order-body">
+                                
+                            </div>
                         </div>
                     <?php endforeach; ?>
                 <?php endif; ?>
