c646c21dfdbf61a274a4942ffc5baaff9b7adb74
redlinka <nachnouchiadam@gmail.com>
2026-01-14 17:26:10 +0100

    Update admin_panel.php

--- Php/img2brick_final/admin_panel.php
+++ Php/img2brick_final/admin_panel.php
@@ -1,15 +1,18 @@
 <?php
-global $cnx;
-require_once __DIR__ . '/i18n.php';
-if (!isset($_SESSION['userId']) || !isset($_SESSION['username'])) {
-    header("Location: connexion.php");
-    exit;
-}
-$navUsername = $_SESSION['username'];
-if ($navUsername != '4DM1N1STRAT0R_4ND_4LM16HTY') {
-    header("Location: index.php");
-    exit;
-}
+    session_start();
+    global $cnx;
+    include("./config/cnx.php");
+    require_once __DIR__ . '/i18n.php';
+
+    if (!isset($_SESSION['userId']) || !isset($_SESSION['username'])) {
+        header("Location: connexion.php");
+        exit;
+    }
+    $navUsername = $_SESSION['username'];
+    if ($navUsername != '4DM1N1STRAT0R_4ND_4LM16HTY') {
+        header("Location: index.php");
+        exit;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
